00e96928250eda42cac94a311be4b703fa99ce99
Jan Czizikow <jan.czizikow@gmail.com>
2022-04-20 18:39:20 +0200

    better decode errors

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -16,6 +19,50 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	return strconv.ParseInt(params.ByName("id"), 10, 64)
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	var syntaxError *json.SyntaxError
+	var unmarshalTypeError *json.UnmarshalTypeError
+	var invalidUnmarshalError *json.InvalidUnmarshalError
+
+	err := json.NewDecoder(r.Body).Decode(dst)
+
+	switch {
+	// Use the errors.As() function to check whether the error has the type *json.SyntaxError.
+	// If it does, then return a plain-english error message
+	// which includes the location of the problem.
+	case errors.As(err, &syntaxError):
+		return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+	// In some circumstances Decode() may also return an io.ErrUnexpectedEOF error // for syntax errors in the JSON.
+	// So we check for this using errors.Is() and
+	// return a generic error message. There is an open issue regarding this at
+	// https://github.com/golang/go/issues/25956.
+	case errors.Is(err, io.ErrUnexpectedEOF):
+		return errors.New("body contains badly-formed JSON")
+
+	// Likewise, catch any *json.UnmarshalTypeError errors. These occur when the
+	// JSON value is the wrong type for the target destination. If the error relates
+	// to a specific field, then we include that in our error message to make it
+	// easier for the client to debug.
+	case errors.As(err, &unmarshalTypeError):
+		if unmarshalTypeError.Field != "" {
+			return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+		}
+		return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+	// An io.EOF error will be returned by Decode() if the request body is empty.
+	// We check for this with errors.Is() and return a plain-english error message instead.
+	case errors.Is(err, io.EOF):
+		return errors.New("body must not be empty")
+
+	case errors.As(err, &invalidUnmarshalError):
+		panic(err)
+
+	default:
+		return err
+	}
 }
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
