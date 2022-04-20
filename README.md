19bff0f3cd3fe97ddd899156cf8698c51b5a21ad
Jan Czizikow <jan.czizikow@gmail.com>
2022-04-20 14:43:38 +0200

    git init

---
+++ README.md
@@ -0,0 +1,5 @@
+# greenlight
+
+Following ["Let's Go Further" book][book] on building RESTful APIs in Go.
+
+[book]: https://lets-go-further.alexedwards.net/ "Let's Go Further book website"
---
+++ cmd/api/helpers.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// any is an alias for interface{} and is equivalent to interface{} in all ways.
+type any = interface{}
+
+// Retrive the "id" URL parameter from the current request context, then convert it to
+// an integer and return it. If the operation isn't successful, return 0 and an error.
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	return strconv.ParseInt(params.ByName("id"), 10, 64)
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+
+	if err != nil {
+		return err
+	}
+
+	// Append a newline to make it easier to view in terminal applications.
+	js = append(js, '\n')
+
+	// At this point, we know that we won't encounter any more errors before writing the
+	// response, so it's safe to add any headers that we want to include. We loop
+	// through the header map and add each header to the http.ResponseWriter header map.
+	// Note that it's OK if the provided header map is nil. Go doesn't throw an error
+	// if you try to range over (or generally, read from) a nil map.
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-type", "application/json")
+
+	w.WriteHeader(status)
+	_, err = w.Write([]byte(js))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
